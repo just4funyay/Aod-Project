4588fbf8773d85986048a19689fca0336a162e20
Tioninta Mandaika Maha <118107093+just4funyay@users.noreply.github.com>
2025-02-07 01:21:23 +0700

    Update urls.py

--- aoddata/urls.py
+++ aoddata/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import InputDatabase,GetRasterDataView
 
 urlpatterns = [
-    path('convert-nc-file/', InputDatabase.as_view(), name='InputDatabase'),
+    path('input-database/', InputDatabase.as_view(), name='InputDatabase'),
      path('tiles/', GetRasterDataView.as_view(), name='raster_tile'),
 ]

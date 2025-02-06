3982c2af5f71c536bf7595d4dc98e2846daf86e0
Tioninta Mandaika Maha <118107093+just4funyay@users.noreply.github.com>
2025-02-07 01:19:46 +0700

    Update models.py

--- aoddata/models.py
+++ aoddata/models.py
@@ -2,4 +2,4 @@ from django.contrib.gis.db import models
 
 class RasterData(models.Model):
     id = models.AutoField(primary_key=True) 
-    raster = models.RasterField(help_text="Data raster GeoTIFF yang disimpan dalam PostGIS")
+    raster = models.RasterField()
